61332cf66bf4650aaa034f6b8e6aa58233847256
Anna Shkolina <annashkolina01@gmail.com>
2023-05-30 23:06:37 +0300

    [feat] added voice recognition for answering the voice messages

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, MessageHandler, filters
 from filter import text_filter
 from secret_token import TOKEN
+from speech_recognition import Speech
 from vectorize import vectorizer, model, INTENTS
+from datetime import datetime
 
 
 def get_intent_ml(user_text):
@@ -40,7 +42,20 @@ async def telegram_reply(upd: Update, ctx):
     print(f"BOT: {reply}")
     await upd.message.reply_text(reply)
 
-handler = MessageHandler(filters.TEXT, telegram_reply)  # Создаем обработчик текстовых сообщений
-app.add_handler(handler)  # Добавляем обработчик в приложение
 
+async def telegram_reply_voice(upd: Update, ctx):
+    name = upd.message.from_user.full_name
+    speech = Speech()
+    user_voice = await upd.message.voice.get_file()
+    user_voice = user_voice.file_path
+    user_voice = speech.recognize(user_voice)
+    print(f"{name}: {user_voice}")
+    reply = bot(user_voice)
+    print(f"BOT: {reply}")
+    await upd.message.reply_text(reply)
+
+handler_text = MessageHandler(filters.TEXT, telegram_reply)  # Создаем обработчик текстовых сообщений
+app.add_handler(handler_text)  # Добавляем обработчик в приложение
+handler_voice = MessageHandler(filters.VOICE, telegram_reply_voice)  # Создаем обработчик голосовых сообщений
+app.add_handler(handler_voice)  # Добавляем обработчик в приложение
 app.run_polling()
